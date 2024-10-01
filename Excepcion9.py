dab5ef4835a0893730cf8cb7cc2caccc6d424bb4
Sebastian <72003459@continental.edu.pe>
2024-10-01 17:17:15 -0500

    novena excepcion

---
+++ Excepcion9.py
@@ -0,0 +1,44 @@
+import random
+
+class Error(Exception):
+    """Clase base para otras excepciones"""
+    pass
+
+class ValueTooSmallError(Error):
+    """Aumenta cuando el valor de entrada es demasiado pequeño"""
+    pass
+
+class ValueTooLargeError(Error):
+    """Aumenta cuando el valor de entrada es demasiado grande"""
+    pass
+
+class InvalidInputError(Error):
+    """Aumenta cuando el valor de entrada no es un número"""
+    pass
+
+# Necesitas adivinar este número
+#random.seed(5)
+number = random.randint(1, 20)
+
+# El usuario adivina un número hasta que lo acierta
+while True:
+    try:
+        i_num = input("Ingrese un número: ")
+        if not i_num.isdigit():
+            raise InvalidInputError
+        i_num = int(i_num)
+        if i_num < number:
+            raise ValueTooSmallError
+        elif i_num > number:
+            raise ValueTooLargeError
+        print("¡Felicidades! Lo adivinaste.")
+        break
+    except ValueTooSmallError:
+        print("Este valor es demasiado pequeño, inténtelo de nuevo.")
+        print()
+    except ValueTooLargeError:
+        print("Este valor es demasiado grande, inténtelo de nuevo.")
+        print()
+    except InvalidInputError:
+        print("Error: El valor de entrada no es un número. Inténtelo de nuevo.")
+        print()
